e84f8a1fc3ce3aee0c0a2a77d1e4a69d3ab18ec5
Ian Chen <ian00022@gmail.com>
2017-05-22 13:27:47 +0800

    test for Cp950 encode

--- dpft-engine-core/src/main/java/com/ibm/dpft/engine/core/util/DPFTCSVFileFormatter.java
+++ dpft-engine-core/src/main/java/com/ibm/dpft/engine/core/util/DPFTCSVFileFormatter.java
@@ -272,7 +272,7 @@ public class DPFTCSVFileFormatter extends DPFTFileFormatter {
 					if(this.getFileEncoding().equals(GlobalConstants.FILE_ENCODE_UTF8)){
 						s = truncateUTF8(s, length);
 						adj_len = getAdjustedLengthUTF8(s, length);
-					}else if(this.getFileEncoding().equals(GlobalConstants.FILE_ENCODE_BIG5)){
+					}else if(this.getFileEncoding().equalsIgnoreCase(GlobalConstants.FILE_ENCODE_BIG5)){
 						s = truncateBig5(s, length);
 						adj_len = getAdjustedLengthBig5(s, length);
 					}
