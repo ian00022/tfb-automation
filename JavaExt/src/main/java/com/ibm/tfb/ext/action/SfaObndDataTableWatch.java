60a0d7750daeb6dbd93d925474bc783b3cb4fa69
Ian Chen <ian00022@gmail.com>
2017-05-23 14:55:48 +0800

    1. fixed issue for big5 to utf8

--- JavaExt/src/main/java/com/ibm/tfb/ext/action/SfaObndDataTableWatch.java
+++ JavaExt/src/main/java/com/ibm/tfb/ext/action/SfaObndDataTableWatch.java
@@ -3,6 +3,13 @@ package com.ibm.tfb.ext.action;
 import com.ibm.dpft.engine.core.action.DPFTActionObndPeriodicDataTableWatch;
 
 public class SfaObndDataTableWatch extends DPFTActionObndPeriodicDataTableWatch {
+	@Override
+	public String getTableWatchCriteria() {
+		StringBuilder sb = new StringBuilder();
+		sb.append(super.getTableWatchCriteria());
+		sb.append(" chal_name='").append(this.getInitialData().getString("chal_name")).append("'");
+		return sb.toString();
+	}
 
 	@Override
 	public String getTableName() {
